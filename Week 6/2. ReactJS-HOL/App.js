4da9b1b76cd8a54e619e2c05703468fac79c51a2
Santhosh kumar S <santhoshssakthivel@gmail.com>
2025-07-25 18:53:24 +0530

    Add files via upload

---
+++ Week 6/2. ReactJS-HOL/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Home from './components/Home';
+import About from './components/about';
+import Contact from './components/contact';
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+      <About />
+      <Contact />
+    </div>
+  );
+}
+
+export default App;
